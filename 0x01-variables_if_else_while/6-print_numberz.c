1e4c7ca5eb2edfcde9f2c89e225141186ac1d780
HosTheRockJohnson <hossamahmedmohmed2892004@gmail.com>
2023-09-08 15:24:19 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - outputs small letters in the alphabet
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+	
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+	}
+	putchar('\n');
+	return (0);
+}

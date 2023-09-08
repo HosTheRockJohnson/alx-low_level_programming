705a8aa1fa6815a08df80d2041dc076764c5e12e
HosTheRockJohnson <hossamahmedmohmed2892004@gmail.com>
2023-09-08 16:29:27 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,19 @@ int main(void)
 {
 	int c = 0;
 
-	for (c / 10 != c % 10 && c / 10 < c % 10)
+	for (c = 1; c <= 99; c++)
 	{
-		putchar(c / 10 + '0');
-		putchar(c % 10 + '0');
-		if (c < 89)
+		if (c / 10 != c % 10 && c / 10 < c % 10)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(c / 10 + '0');
+			putchar(c % 10 + '0');
+			if (c < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-}
 	putchar('\n');
 	return (0);
 }

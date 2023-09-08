6c33e6a77fbf84e321bd903c864d616094de091b
HosTheRockJohnson <hossamahmedmohmed2892004@gmail.com>
2023-09-08 15:04:03 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - outputs small letters in the alphabet
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

5f4d7e5bcd871ddf4f4d372211007d50c3a74852
HosTheRockJohnson <hossamahmedmohmed2892004@gmail.com>
2023-09-13 17:43:37 -0700

    plz

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - main
+ * Return: times_table
+ */
+
+void times_table(void)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar('0' + d);
+				_putchar('0' + u);
+			}
+			else 
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar('0' + z);
+			}
+		}
+		_putchar('\n');
+	}
+}

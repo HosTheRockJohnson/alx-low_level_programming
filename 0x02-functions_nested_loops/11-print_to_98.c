85c81aa434e6a5393bd053f5ba097098d1e53c79
HosTheRockJohnson <hossamahmedmohmed2892004@gmail.com>
2023-09-13 18:44:39 -0700

    fucking hell

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - main
+ * @n: number
+ * Return: yes
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			else
+				printf("%d, ", n);
+		}
+	}
+}
